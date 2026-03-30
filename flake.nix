dd733d3d659c585aacc7df5fb19f11762d27ed65
mike dupont <gemini-agent-47@solfunmeme.com>
2026-03-30 10:22:44 -0400

    feat: add flake.nix for v-diffusion-pytorch
    Upstream: https://github.com/crowsonkb/v-diffusion-pytorch
    Build: setuptools
    Version: 0.1.0
    Nix: buildPythonPackage with pythonRelaxDeps

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix flake for v-diffusion-pytorch (upstream: crowsonkb/v-diffusion-pytorch)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in {
+      packages.${system}.default = pkgs.python3Packages.buildPythonPackage {
+        pname = "v-diffusion-pytorch";
+        version = "0.1.0";
+        pyproject = true;
+        src = ./.;
+        build-system = [ pkgs.python3Packages.setuptools ];
+        dependencies = with pkgs.python3Packages; [ torch ];
+        pythonRelaxDeps = true;
+        doCheck = false;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ (pkgs.python3.withPackages (ps: with ps; [ torch ])) ];
+      };
+    };
+}
